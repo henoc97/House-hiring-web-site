ed3a13e44017976e69ca168080e919ff39b554be
Hénoc <amaviganhenoc@gmail.com>
2024-11-30 23:37:41 +0000

    remove redirect server config

--- config/ecosystem.config.js
+++ config/ecosystem.config.js
@@ -3,22 +3,22 @@ require('dotenv').config();
 
 module.exports = {
   apps: [
+    // {
+    //   name: 'house-hiring-web-site-http-redirect',
+    //   script: 'src/redirectServer.js', //'src/server.js',
+    //   instances: 1,
+    //   exec_mode: 'cluster', // Activer le mode de clustering
+    //   env_production: {
+    //     NODE_ENV: 'production',
+    //     PORT: process.env.PORT,
+    //   },
+    //   env_development: {
+    //     NODE_ENV: 'development',
+    //     PORT: process.env.PORT,
+    //   },
+    // },
     {
-      name: 'house-hiring-web-site-http-redirect',
-      script: 'src/redirectServer.js', //'src/server.js',
-      instances: 1,
-      exec_mode: 'cluster', // Activer le mode de clustering
-      env_production: {
-        NODE_ENV: 'production',
-        PORT: process.env.PORT,
-      },
-      env_development: {
-        NODE_ENV: 'development',
-        PORT: process.env.PORT,
-      },
-    },
-    {
-      name: 'house-hiring-web-site-https-server',
+      name: 'house-hiring-web-site-http-server',
       script: 'src/server.js', //'src/server.js',
       instances: 1,
       exec_mode: 'cluster', // Activer le mode de clustering
